8777a594e1626f896b79a6028093b766c4d986af
Vansh16aug <kumarvansh16aug@gmail.com>
2024-11-16 00:21:25 +0530

    added

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+
+$id = $_GET['rn'];
+try {
+    $sql = "DELETE FROM CHATBOT_HINTS WHERE ID = :id";
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    
+    if ($stmt->execute()) {
+        echo "<script>alert('Record Deleted from database');</script>";
+    } else {
+        echo "<font color='red'>Failed to delete from db!</font>";
+    }
+
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage());
+}
+
+?>
+
+<META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://localhost:7882/qna.php">
